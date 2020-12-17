73bd0e86feae47b2ea6b94623f984b6cb153689a
danche <danche@mi.com>
2020-12-17 11:50:40 +0800

    record: 06-dispatching-to-a-class

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -2,13 +2,22 @@
 
 namespace App;
 
+use FastRoute\RouteCollector;
+use Http\HttpRequest;
+use Http\HttpResponse;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-error_reporting(E_ALL);
+$log = new Logger('name');
+$file_log = $_SERVER['DOCUMENT_ROOT'].'/../storage/logs/'.date('Y-m-d') . '_info.log';
+$log->pushHandler(new StreamHandler($file_log, Logger::INFO));
+$log->info(__FILE__.'==>'.__LINE__.'==',[1111]);
 
+error_reporting(E_ALL);
 $environment = 'development';
 
 /**
@@ -24,18 +33,34 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-//throw new \Exception;
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$request = new HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new HttpResponse();
 
-$response = new \Http\HttpResponse;
-foreach ($response->getHeaders() as $header) {
-    header($header, false);
+$routeDefinitionCallback = function (RouteCollector $r) {
+    $routes = include('Routes.php');
+    foreach ($routes as $route) {
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+};
+
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->setStatusCode(404);
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $className = $routeInfo[1][0];
+        $method = $routeInfo[1][1];
+        $vars = $routeInfo[2];
+
+        $class = new $className;
+        $class->$method($vars);
+        break;
 }
-
-$content = '<h1>Hello World</h1>';
-$response->setContent($content);
-echo $response->getContent();
-
-//$response->setContent('404 - Page not found');
-//$response->setStatusCode(404);
-//echo $response->getContent();
